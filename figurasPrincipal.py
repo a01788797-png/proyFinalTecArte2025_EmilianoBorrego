4122e74a62ee66890e93f3db7340afd6073cdd73
Emiliano <A01788797@tec.mx>
2025-11-02 18:06:36 -0600

    Esta es la versión final de nuestro código.

--- figurasPrincipal.py
+++ figurasPrincipal.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import math
+from funciones import triangulo, rectangulo, circulo
 
 dataFile = pd.read_csv("figuras.csv")
 
@@ -9,4 +10,17 @@ areas = []
 perimetros = []
 
 for index, row in dataFile.iterrows():
-	print(f"Fila {index}: FIGURA={row['FIGURA']}, Medida1={row['MEDIDA1']}, Medida2={row['MEDIDA2]}")
+
+    if row['FIGURA'] == 't':
+        print(f"Fila {index}: FIGURA={row['FIGURA']}, Medida1={row['MEDIDA1']}, Medida2={row['MEDIDA2']}")
+        area = triangulo(row['MEDIDA1'],row['MEDIDA2'])
+    elif row['FIGURA'] == 'r':
+        print(f"Fila {index}: FIGURA={row['FIGURA']}, Medida1={row['MEDIDA1']}, Medida2={row['MEDIDA2']}")
+        area = rectangulo(row['MEDIDA1'],row['MEDIDA2'])
+    elif row['FIGURA'] == 'c':
+        print(f"Fila {index}: FIGURA={row['FIGURA']}, Medida1={row['MEDIDA1']}, Medida2={row['MEDIDA2']}")
+        area = circulo(row['MEDIDA1'], row['MEDIDA2'])
+
+            
+
+       
